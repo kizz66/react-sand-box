0787c18a6f2d3bc6816b2d9c8a8187e57925b32f
igor <kizz66@mail.ru>
2018-06-15 01:19:36 +0700

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
                     fallback: 'style-loader',
                     use: ['css-loader', 'sass-loader']
                 })
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             }
         ]
     },
