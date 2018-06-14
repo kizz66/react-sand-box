bd228958933dc6408a6ce725ed4fd1dac2ec63fa
igor <kizz66@mail.ru>
2018-06-15 02:45:55 +0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'semantic-ui-react';
+import {Button, Form, Grid} from 'semantic-ui-react';
 
 import './base.scss';
 import './semant-ui-styles/semantic.scss';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: "777"};
+    }
+    handleSubmit(e, form) {
+        console.log(form);
+        console.log(e.target);
+    }
+
     render() {
         return (
-            <div>
-                
-               <Button>click</Button>
-            </div>
+            <Form onSubmit={this.handleSubmit}>
+                <Grid columns={3}>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Form.Field>
+                                <label>First Name</label>
+                                <input placeholder='First Name' value={this.state.name}/>
+                            </Form.Field>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Button type='submit'>Submit</Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Form>
         )
     }
 }
