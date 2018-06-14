6c79c2678ac528db5267d08b4f52f380722665f9
igor <kizz66@mail.ru>
2018-06-14 19:46:16 +0700

    wip

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * i.kiz kizz66@mail.ru
+ * Date: 14.06.2018
+ */
+var path = require('path');
+var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/index.html'
+    })]
+}
